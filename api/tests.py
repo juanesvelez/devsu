dbd6a1869fb21396110298c43ca6ee5be9623bbb
Juan Esteban Vélez Muñoz <juanesvelez@gmail.com>
2024-07-02 12:33:20 -0500

    Prueba tecnica - unit test

--- api/tests.py
+++ api/tests.py
@@ -6,11 +6,12 @@ from .models import User
 class TestUserView(APITestCase):
     def setUp(self):
         self.user = User.objects.create(name='Test1', dni='09876543210')
-        self.url = reverse("user-list")
+        self.url_list = reverse("user-list")  # Basename 'user'
+        self.url_detail = reverse("user-detail", args=[self.user.id])  # 'user-detail'
         self.data = {'name': 'Test2', 'dni': '09876543211'}
 
     def test_post(self):
-        response = self.client.post(self.url, self.data, format='json')
+        response = self.client.post(self.url_list, self.data, format='json')
         self.assertEqual(response.status_code, 201)
         self.assertEqual(
             json.loads(response.content),
@@ -19,12 +20,12 @@ class TestUserView(APITestCase):
         self.assertEqual(User.objects.count(), 2)
 
     def test_get_list(self):
-        response = self.client.get(self.url)
+        response = self.client.get(self.url_list)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(json.loads(response.content)), 1)
 
     def test_get(self):
-        response = self.client.get(reverse("user-detail", args=[self.user.id]))
+        response = self.client.get(self.url_detail)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
             json.loads(response.content),
